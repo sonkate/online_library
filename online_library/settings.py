4b677b1f777c997f64aeb7babcf9c35c49f547e3
SonThee <theson2604@gmail.com>
2023-11-25 14:55:49 +0700

    add allowed hosts

--- online_library/settings.py
+++ online_library/settings.py
@@ -55,9 +55,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOWED_ORIGINS = [     "http://localhost:8000",     "http://localhost:3000" ]
 
 ROOT_URLCONF = 'online_library.urls'
 
